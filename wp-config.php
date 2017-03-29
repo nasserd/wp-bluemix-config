edbca211efc7f7bd6e2a8a22cae8d5140601729b
Nasser Dassi (antideas) <nasserd@antideas.com>
2017-03-29 02:03:45 -0700

    Adding WP Multisite Variable

--- wp-config.php
+++ wp-config.php
@@ -114,6 +114,9 @@ define('WPLANG', '');
  */
 define('WP_DEBUG', false);
 
+/* Multisite */
+define( 'WP_ALLOW_MULTISITE', true );
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
